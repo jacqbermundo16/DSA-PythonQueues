9064bab5072ecfdbb3572b2a69ecbe6db24d7c3d
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-12-17 03:15:50 +0800

    Modifyin the code so that is accepts an optional sorting strategy

--- graph.py
+++ graph.py
@@ -31,18 +31,20 @@ def load_graph(filename, node_factory):
         for name1, name2, weights in graph.edges(data=True)
     )
 
-def breadth_first_traverse(graph, source):
+def breadth_first_traverse(graph, source, order_by=None):
     queue = Queue(source)
     visited = {source}
     while queue:
         yield (node := queue.dequeue())
-        for neighbor in graph.neighbors(node):
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.enqueue(neighbor)
+        neighbor = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
+             visited.add(neighbor)
+             queue.enqueue(neighbor)
 
-def breatdth_first_search(graph, source, predicate):
-    for node in breadth_first_traverse(graph, source):
+def breatdth_first_search(graph, source, predicate, order_by=None):
+    for node in breadth_first_traverse(graph, source, order_by):
         if predicate(node):
             return node
 
