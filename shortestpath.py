314dd91becc657bcb33017f89b52830b80d32668
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-12-17 03:20:54 +0800

    Revealing all the shortest paths between two cities

---
+++ shortestpath.py
@@ -0,0 +1,9 @@
+import networkx as nx
+from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i in path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", "→" .join(city.name for city in path))
