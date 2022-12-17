7579f03819097f3754703ea058dbc4dc9746fad3
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-12-17 17:14:30 +0800

    Implemeting dijkstra algorithm

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 from typing import NamedTuple
 import networkx as nx
-from queues import Queue, Stack
+from math import inf as infinity
+from queues import MutableMinHeap, Queue, Stack
 from collections import deque
 
 
@@ -116,3 +117,24 @@ def search(traverse, graph, source, predicate, order_by=None):
     for node in traverse(graph, source, order_by):
         if predicate(node):
             return node
+
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
