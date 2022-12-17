7194f1512a3f21fdc115e17f48daf9e36670350f
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-12-17 16:22:52 +0800

    Calling the queue-bases implementation of the shortest path that will get the same result as with network

--- shortestpath.py
+++ shortestpath.py
@@ -1,9 +1,13 @@
-import networkx as nx
-from graph import City, load_graph
+#import networkx as nx
+#from graph import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 city1 = nodes["aberdeen"]
 city2 = nodes["perth"]
 
-for i in path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
-    print(f"{i}.", "→" .join(city.name for city in path))
+#for i in path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+#    print(f"{i}.", "→" .join(city.name for city in path))
+
+
+from graph import shortest_path
+"→".join(city.name for city in shortest_path(graph, city1, city2))
